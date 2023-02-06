dfc7400811ca5d4be6011e49b9a6a77218242484
SecretAgent <iyengar.dhruv@outlook.com>
2023-02-06 10:08:50 -0800

    added player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move);
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
